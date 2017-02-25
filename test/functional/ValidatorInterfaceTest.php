b1b0471d76011ddad7698e1fb98a4f6dc0e0ff1e
Anton Ukhanev <xedin.unknown@gmail.com>
2017-02-25 10:11:47 +0100

    Centralized test subject class name

--- test/functional/ValidatorInterfaceTest.php
+++ test/functional/ValidatorInterfaceTest.php
@@ -2,13 +2,22 @@
 
 namespace Dhii\Validation;
 
+use Xpmock\TestCase;
+
 /**
  * Tests {@see Dhii\Validation\ValidatorInterface}.
  *
  * @since [*next-version*]
  */
-class ValidatorInterfaceTest extends \Xpmock\TestCase
+class ValidatorInterfaceTest extends TestCase
 {
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\\Validation\\ValidatorInterface';
+
     /**
      * Creates a new instance of the test subject.
      *
@@ -18,14 +27,14 @@ class ValidatorInterfaceTest extends \Xpmock\TestCase
      */
     public function createInstance()
     {
-        $mock = $this->mock('Dhii\Validation\ValidatorInterface')
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
                 ->new();
 
         return $mock;
     }
 
     /**
-     * Tests whether a valid instance can be created.
+     * Tests whether a valid instance of the test subject can be created.
      *
      * @since [*next-version*]
      */
@@ -33,6 +42,6 @@ class ValidatorInterfaceTest extends \Xpmock\TestCase
     {
         $subject = $this->createInstance();
 
-        $this->assertInstanceOf(Dhii\Validation\ValidatorInterface, $subject, 'Could not create a valid instance');
+        $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject, 'Could not create a valid instance');
     }
 }
