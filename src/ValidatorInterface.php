d3a82eaa72d9053e91e380964fd6bc4c1cfe5d7c
Anton Ukhanev <xedin.unknown@gmail.com>
2017-02-25 10:06:10 +0100

    Improved documentation
    - Added param doc;
    - Added throws doc;
    - Shortened return doc.

--- src/ValidatorInterface.php
+++ src/ValidatorInterface.php
@@ -2,6 +2,8 @@
 
 namespace Dhii\Validation\ValidatorInterface;
 
+use Dhii\Util\String\StringableInterface;
+
 /**
  * Something that can validate a value.
  *
@@ -14,9 +16,13 @@ interface ValidatorInterface
      *
      * @since [*next-version*]
      *
-     * @return string[]|\Dhii\Util\String\StringableInterface[]|\Traversable A list of errors.
-     *                                                                       If the list is empty, indicates that no validation errors occurred, and the value is valid.
-     *                                                                       Each error is a string, or an object that can be cast to string.
+     * @param mixed $value The subject of validation.
+     *
+     * @throws ValidationExceptionInterface If problem validating.
+     *
+     * @return string[]|StringableInterface[]|\Traversable A list of errors.
+     *                                                     If the list is empty, indicates that no validation errors occurred, and the value is valid.
+     *                                                     Each error is a string, or an object that can be cast to string.
      */
     public function validate($value);
 }
