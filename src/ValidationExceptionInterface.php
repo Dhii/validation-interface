0f67c8b39a4af26f6282786e080ae122c97ee188
Anton Ukhanev <xedin.unknown@gmail.com>
2017-02-25 10:05:48 +0100

    Added exception interfaces

---
+++ src/ValidationExceptionInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dhii\Validation\ValidatorInterface;
+
+/**
+ * Something that can represent an exception which can occur in or be related to
+ * a validation process or component.
+ *
+ * @since [*next-version*]
+ */
+interface ValidationExceptionInterface
+{
+}
---
+++ src/ValidationFailedException.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Dhii\Validation\ValidatorInterface;
+
+use Dhii\Util\String\StringableInterface;
+
+/**
+ * Something that can represent an exception which occurs if a validation
+ * fails.
+ *
+ * @since [*next-version*]
+ */
+interface ValidationFailedException extends ValidationExceptionInterface
+{
+    /**
+     * @since [*next-version*]
+     *
+     * @return string[]|StringableInterface[]|\Traversable A list of errors.
+     *                                                     Each error is something that can be treated as a string, and represents
+     *                                                     a description of why a validation subject is invalid.
+     */
+    public function getValidationErrors();
+}
