44bdbf72a1a013cf21f4644fe943a23b9c65a87f
Anton Ukhanev <xedin.unknown@gmail.com>
2017-02-25 10:12:09 +0100

    Added `validate()` method to mock

--- test/functional/ValidatorInterfaceTest.php
+++ test/functional/ValidatorInterfaceTest.php
@@ -28,6 +28,7 @@ class ValidatorInterfaceTest extends TestCase
     public function createInstance()
     {
         $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+                ->validate()
                 ->new();
 
         return $mock;
