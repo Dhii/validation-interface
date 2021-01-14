f4db27de09da202db69ff4842a51abd48489ef25
Anton Ukhanev <xedin.unknown@gmail.com>
2021-01-14 17:05:06 +0100

    Remove redundant exception class

--- src/ValidatorInterface.php
+++ src/ValidatorInterface.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Dhii\Validation;
 
-use Dhii\Validation\Exception\ValidationExceptionInterface;
 use Dhii\Validation\Exception\ValidationFailedExceptionInterface;
+use RuntimeException;
 
 /**
  * Something that can validate a value.
@@ -17,8 +17,8 @@ interface ValidatorInterface
      *
      * @param mixed $value The subject of validation.
      *
-     * @throws ValidationExceptionInterface       If problem validating.
-     * @throws ValidationFailedExceptionInterface If validation failed.
+     * @throws RuntimeException                                    If problem validating.
+     * @throws ValidationFailedExceptionInterface                  If validation failed. Must extend {@see RuntimeException}.
      */
     public function validate($value): void;
 }
